b09f7c442d8d32ebf4bf5c01632f3f1ad0584f5a
Robert-Rino <a901002666@gmail.com>
2017-07-31 11:01:58 +0800

    add nomed package

---
+++ nomed.py
@@ -0,0 +1,15 @@
+import requests
+from geopy.distance import vincenty
+
+r = requests.get('https://cafenomad.tw/api/v1.2/cafes')
+stores = r.json()
+
+class Nomed:
+    @classmethod
+    def findByGeo(cls, latitude, longitude):
+        start = (latitude, longitude)
+        near_store = sorted(stores, key= lambda x :cls.calculateDistance(start, (float(x['latitude']), float(x['longitude']))))
+        return near_store
+
+    def calculateDistance(_from, _to):
+        return vincenty(_from, _to).miles
