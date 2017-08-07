24f4e2e16d283a0155a9765e288b818de7c5bb1d
Robert-Rino <a901002666@gmail.com>
2017-08-07 23:16:48 +0800

    add dummy contents for product

---
+++ products.py
@@ -0,0 +1,5 @@
+
+products = [
+{'picture_url':'https://i.ytimg.com/vi/mXnK05e2lKg/maxresdefault.jpg', 'name':'product1', 'price':'1'},
+{'picture_url':'https://i.ytimg.com/vi/mXnK05e2lKg/maxresdefault.jpg', 'name':'product2', 'price':'2'},
+{'picture_url':'https://i.ytimg.com/vi/mXnK05e2lKg/maxresdefault.jpg', 'name':'product3', 'price':'3'}]
