7e54bbae93e49f252275e4db55354b31547272b7
Robert-Rino <a901002666@gmail.com>
2017-08-15 01:12:55 +0800

    update product list content

--- products.py
+++ products.py
@@ -1,5 +1,5 @@
 
 products = [
-{'picture_url':'https://i.ytimg.com/vi/mXnK05e2lKg/maxresdefault.jpg', 'name':'product1', 'price':'1'},
-{'picture_url':'https://i.ytimg.com/vi/mXnK05e2lKg/maxresdefault.jpg', 'name':'product2', 'price':'2'},
-{'picture_url':'https://i.ytimg.com/vi/mXnK05e2lKg/maxresdefault.jpg', 'name':'product3', 'price':'3'}]
+{'p_id':0, 'market_url':'https://www.obdesign.com.tw/product.aspx?seriesID=KL0265-&no=7032', 'picture_url':'https://obcdn2.obdesign.com.tw/catalog/170405tbw/KL0265.jpg', 'name':'product1', 'price':'1'},
+{'p_id':1, 'market_url':'https://www.obdesign.com.tw/product.aspx?seriesID=KL0093&no=7032', 'picture_url':'https://obcdn2.obdesign.com.tw/catalog/160101eli/KL0093.jpg', 'name':'product2', 'price':'2'},
+{'p_id':2, 'market_url':'https://www.obdesign.com.tw/product.aspx?seriesID=SD0091&no=10311', 'picture_url':'https://obcdn2.obdesign.com.tw/catalog/170502rpf/SD0091.jpg', 'name':'product3', 'price':'3'}]
