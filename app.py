d207e298e1bd3bf68fe5591ab4bca68ead83064c
Robert-Rino <a901002666@gmail.com>
2017-09-27 23:06:32 +0800

    add follow,unfollow event handler , send_bind route

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, PostbackEvent,
+    MessageEvent, PostbackEvent, FollowEvent, UnfollowEvent,
     TextMessage, TextSendMessage, StickerSendMessage,
     LocationMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn,
+    ConfirmTemplate, ImageCarouselColumn,
     PostbackTemplateAction, MessageTemplateAction, URITemplateAction
 )
 from nomed import Nomed
@@ -89,6 +90,73 @@ def buildProductColumnsForUser(products, userId):
     ) for product in products]
     return result
 
+@app.route('/image_carouse', methods=['POST'])
+def image_carouse():
+    data = request.json
+    userId = data['user_id']
+    import products as product_example
+    # products = data['products']
+    products = product_example
+
+    if len(products) == 0 :
+        return jsonify({'message': 'bad request'}), 400
+
+    else:
+        product_list = buildImageColumnsForUser(products, userId)[0:4]
+        line_bot_api.push_message(
+            userId,TemplateSendMessage(
+                alt_text='您的專屬配件',
+                template=CarouselTemplate(
+                columns=[
+                    product for product in product_list
+                ]
+                )
+            )
+        )
+        return jsonify({'message' : 'ok'}), 200
+
+def buildImageColumnsForUser(products, userId):
+    result = [ImageCarouselColumn(
+        thumbnail_image_url=product['picture_url'],
+        title=product['name'],
+        text="$ {}".format(product['price']),
+        actions=[
+            URITemplateAction(
+                label='前往賣場',
+                uri=product['target_url'])
+        ]
+    ) for product in products]
+    return result
+
+@app.route('/send_binding', methods=['POST'])
+def bindLineuserTagtoouser():
+    data = request.json
+    userid_list = data['userid_list']
+
+    for userid in userid_list:
+        line_bot_api.push_message(
+            userid,
+            TemplateSendMessage(
+                alt_text='打開手機看看有什麼新商品吧',
+                template=ConfirmTemplate(
+                text='想看看有什麼新商品嗎',
+                actions=[
+                    URITemplateAction(
+                    label='前往賣場看看',
+                    uri='https://api-rino-demo-dot-tagtoo-staging.appspot.com/v1/ad/click?u=https://www.obdesign.com.tw/&line_cid=0001&line_uid={}'.format(userid)
+                    ),
+                    PostbackTemplateAction(
+                        label='新鮮推薦',
+                        data="user_id={}&command=recommendation".format(userid),
+                    ),
+                ]
+                )
+            )
+
+        )
+
+    return 'ok'
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     app.logger.info("in echo message handler, event = {} ".format(event.type))
@@ -141,11 +209,47 @@ def buildStoreCarouselColumns(stores):
 def handlePostback(event):
     data = parsePostbackDataString(event.postback.data)
     print(data)
+
     return 'ok', 200
 
 def parsePostbackDataString(postbackDataStirng):
     dataPairs = postbackDataStirng.split('&')
     return { dataPair.split('=')[0]: dataPair.split('=')[1] for dataPair in dataPairs}
+
+# class PostbackSwitcher():
+#
+#     def __init__(self, postback_data):
+#         self.user_id = postback_data.user_id
+#         self.command = postback_data.command
+
+
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    line_bot_api.push_message(
+        event.source.user_id,
+        TemplateSendMessage(
+            alt_text='打開手機看看有什麼新商品吧',
+            template=ConfirmTemplate(
+            text='想看看有什麼新商品嗎',
+            actions=[
+                URITemplateAction(
+                label='前往賣場看看',
+                uri='https://api-rino-demo-dot-tagtoo-staging.appspot.com/v1/ad/click?u=https://www.obdesign.com.tw/&line_cid=0001&line_uid={}'.format(userid)
+                ),
+                MessageTemplateAction(
+                    label='message',
+                    text='message text'
+                )
+            ]
+            )
+        )
+
+    )
+
+@handler.add(UnfollowEvent)
+def handle_follow(event):
+    app.logger.info("in unfollow event userId:{}".format(event.source.user_id))
 
 @handler.default()
 def default(event):
