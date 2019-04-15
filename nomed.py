90cd5459a1db904c621c0fd7f263a69ff8052224
rino_chang <rino_chang@trendmicro.com>
2019-04-16 01:08:44 +0800

    make static  method

--- nomed.py
+++ nomed.py
@@ -7,9 +7,9 @@ stores = r.json()
 class Nomed(object):
     @staticmethod
     def findByGeo(latitude, longitude):
-         def calculateDistance(_from, _to):
+        def calculateDistance(_from, _to):
             return vincenty(_from, _to).miles
-            
+
         start = (latitude, longitude)
         near_store = sorted(stores, key= lambda x :calculateDistance(start, (float(x['latitude']), float(x['longitude']))))
         return near_store
