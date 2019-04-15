5e710063c278288ceb9b86661100087a36fc50d3
rino_chang <rino_chang@trendmicro.com>
2019-04-16 01:03:34 +0800

    make static  method

--- nomed.py
+++ nomed.py
@@ -4,12 +4,12 @@ from geopy.distance import vincenty
 r = requests.get('https://cafenomad.tw/api/v1.2/cafes')
 stores = r.json()
 
-class Nomed:
-    @classmethod
+class Nomed(object):
+    @staticmethod
     def findByGeo(cls, latitude, longitude):
         start = (latitude, longitude)
         near_store = sorted(stores, key= lambda x :cls.calculateDistance(start, (float(x['latitude']), float(x['longitude']))))
         return near_store
-    @classmethod
+    @staticmethod
     def calculateDistance(_from, _to):
         return vincenty(_from, _to).miles
