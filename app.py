13537d30e0a3f67267c29592abcaa28b907c8f23
rino_chang <rino_chang@trendmicro.com>
2019-04-17 00:28:12 +0800

    test

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def CarouselColumnBuilder(stores):
                     ),
             URIAction(
                         label='google map',
-                        uri="https://www.google.com.tw/maps/place/")
+                        uri="https://www.google.com.tw/maps/place/"
                     )
         ]
     ) for store in stores]
