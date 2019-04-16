a1342b8ec1b6ebf87fc043fce3dae4e25f680776
rino_chang <rino_chang@trendmicro.com>
2019-04-16 23:45:32 +0800

    error handling for calculating distance

--- nomed.py
+++ nomed.py
@@ -8,9 +8,18 @@ class Nomed(object):
     @staticmethod
     def findByGeo(latitude, longitude):
         def calculateDistance(_from, _to):
-            return vincenty(_from, _to).miles
+            try:
+                distance = vincenty(_from, _to).miles
+            except Exception:
+                return 10**10
+            return distance
 
         start = (latitude, longitude)
         near_store = sorted(stores, key= lambda x :calculateDistance(start, (float(x['latitude']), float(x['longitude']))))
         return near_store
 
+if __name__ == '__main__':
+    # for store in stores:
+    #     if (float(store['latitude']) > float(90)) or (float(store['latitude']) <= float(-90)):
+    #         print (float(store['latitude']), float(store['longitude']))
+    print Nomed.findByGeo(24.7847536, 120.9901789)
