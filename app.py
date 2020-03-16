13f5a989f246075551d927ce3ff38dae0678be61
ninochang <nino.chang@swag.live>
2020-03-16 10:18:17 +0800

    log host

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # print 'request host: {host}'.format(request.host)
+    print 'request host: {host}'.format(host=request.host)
     return 'Hello World!'
 
 if __name__ == '__main__':
